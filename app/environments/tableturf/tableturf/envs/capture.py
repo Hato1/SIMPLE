76eb36f485f4a94256d813d6f96c09cedaade1e8
Jacob Rogers <jacob.p.r@hotmail.com>
2023-02-15 01:12:29 +1300

    Testing capture card

---
+++ app/environments/tableturf/tableturf/envs/capture.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2 as cv
+
+cap = cv.VideoCapture(1)
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+frame_rate = cap.get(cv.CAP_PROP_FPS)
+
+# Define the codec and create VideoWriter object
+fourcc = cv.VideoWriter_fourcc(*'H264')
+out = cv.VideoWriter('output.avi', fourcc, frame_rate, (width,  height))
+
+
+while cap.isOpened():
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    # Our operations on the frame come here
+    # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # Display the resulting frame
+    cv.imshow('frame', frame)
+    out.write(frame)
+    if cv.waitKey(1) == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+out.release()
+cv.destroyAllWindows()
